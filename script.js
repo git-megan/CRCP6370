230c0002938c8a13b6cb2d476382fcc28a8e5a93
Megan Middleton <uxtechnologist@gmail.com>
2024-09-26 13:45:23 -0700

    Persist chat history by using model.startChat()

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const apiKey = "MY_KEY";
+const apiKey = ;
 
 const genAI = new GoogleGenerativeAI(apiKey);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
+// using model.startChat will persist the chat history
+// see docs: https://ai.google.dev/gemini-api/docs/text-generation?lang=node
+const chat = model.startChat({
+  history: [
+    {
+      role: "user",
+      parts: [{ text: "Hi" }],
+    },
+  ],
+});
+
 const sendButton = document.getElementById("send-button");
 sendButton.addEventListener("click", sendMessage);
-
-let lastMessage = ""; // keep track of conversation history with last message reply from the bot
 
 async function sendMessage() {
   // get user input and display it
@@ -17,15 +26,12 @@ async function sendMessage() {
 
   // call api for data based on the prompt & the user's input
   const prompt =
-    "This is was what you last told the user: " +
-    lastMessage +
-    "As a friendly storyteller, poet, and tarot reader, concisely reply to: "; // to customize the interaction
-  const result = await model.generateContent(prompt + userInput);
+    "As a friendly storyteller, poet, and tarot reader, concisely reply to: ";
+  const result = await chat.sendMessage(prompt + userInput);
 
   // get bot's reply from the result fetched
 
   const botReply = result.response.text();
-  lastMessage = botReply;
   appendMessage("bot", botReply);
 
   // clear the input form so user can write something else
